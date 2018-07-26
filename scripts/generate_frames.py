77ccf23464c269c2c2439e1065d548c73d7f73f7
Bryan Brzycki <bryanb@blpc1.bl.pvt>
2018-07-26 22:30:46 +0000

    Edit scripts to generate and train on images with different normalizations

---
+++ scripts/generate_frames.py
@@ -0,0 +1,130 @@
+# imports
+import numpy as np
+from blimpy import read_header, Waterfall, Filterbank
+
+import matplotlib.pyplot as plt
+
+import sys, os, glob
+sys.path.append("../../setigen")
+import setigen as stg
+
+# Set image parameters
+tsamp = 18.253611008
+fch1 = 6095.214842353016
+df = -2.7939677238464355e-06
+
+fchans = 1024
+tchans = 16
+
+fs = np.arange(fch1, fch1 + fchans*df, df)
+ts = np.arange(0, tchans*tsamp, tsamp)
+
+# Create folders
+import errno
+dir = 'normalized_comparison'
+sets = ['raw', 'normalized', 'normalized_excluded']
+for set in sets:
+    # labels = ['pulsed_nonzero', 'pulsed_zero', 'constant_nonzero', 'constant_zero', 'noise']
+    labels = ['pulsed', 'constant', 'noise']
+    dirs = ['/datax/scratch/bbrzycki/data/%s/%s/train/%s/' % (dir, set, label) for label in labels] \
+            + ['/datax/scratch/bbrzycki/data/%s/%s/validation/%s/' % (dir, set, label) for label in labels] 
+
+    for d in dirs:
+        try:
+            os.makedirs(d)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+                
+# Generation
+# Generate training and validation data!
+datasets = [('train', 5000), ('validation', 500)]
+
+for set in sets:
+    for name, num in datasets:
+
+        for i in range(num):
+
+            output_fn = '/datax/scratch/bbrzycki/data/%s/%s/%s/%s/%s_%04d.png' % (dir,set,name,'pulsed','pulsed',i)
+
+            start_index = np.random.randint(0,fchans)
+            drift_rate = np.random.uniform(-start_index*df/(tsamp*tchans),
+                                           (fchans-1-start_index)*df/(tsamp*tchans))
+            #drift_rate = 0
+            level = np.random.uniform(2, 5)
+            width = np.random.uniform(0.02, 0.05) ** 3
+            
+            amplitude = np.random.uniform(level/4, level)
+            # amplitude = level
+            period = np.random.uniform(50, 100)
+            # width = np.random.uniform(0.000009, 0.000225)
+            
+
+            signal = stg.generate(ts,
+                                      fs,
+                                      stg.constant_path(f_start = fs[start_index], drift_rate = drift_rate),
+                                      stg.sine_t_profile(period = period,
+                                                         phase = 0,
+                                                         amplitude = amplitude,
+                                                         level = level),
+                                      stg.gaussian_f_profile(width = width),
+                                      stg.constant_bp_profile(level = 1.0))
+
+            if set == 'raw':
+                signal = signal
+            elif set == 'normalized':
+                signal = stg.normalize(stg.inject_noise(signal), cols = 0, exclude = 0.0, use_median=False)
+            elif set == 'normalized_excluded':
+                signal = stg.normalize(stg.inject_noise(signal), cols = 1, exclude = 0.2, use_median=False)
+
+            plt.imsave(output_fn, signal)
+            print('Saved %s of %s pulsed data for %s, set %s' % (i, num, name, set))
+
+        for i in range(num):
+
+            output_fn = '/datax/scratch/bbrzycki/data/%s/%s/%s/%s/%s_%04d.png' % (dir,set,name,'constant','constant',i)
+
+            start_index = np.random.randint(0,fchans)
+            drift_rate = np.random.uniform(-start_index*df/(tsamp*tchans),
+                                           (fchans-1-start_index)*df/(tsamp*tchans))
+            level = np.random.uniform(2, 5)
+            width = np.random.uniform(0.02, 0.05) ** 3
+
+            signal = stg.generate(ts,
+                                      fs,
+                                      stg.constant_path(f_start = fs[start_index], drift_rate = drift_rate),
+                                      stg.constant_t_profile(level = level),
+                                      stg.gaussian_f_profile(width = width),
+                                      stg.constant_bp_profile(level = 1.0))
+
+            if set == 'raw':
+                signal = signal
+            elif set == 'normalized':
+                signal = stg.normalize(stg.inject_noise(signal), cols = 0, exclude = 0.0, use_median=False)
+            elif set == 'normalized_excluded':
+                signal = stg.normalize(stg.inject_noise(signal), cols = 1, exclude = 0.2, use_median=False)
+
+            plt.imsave(output_fn, signal)
+            print('Saved %s of %s constant data for %s, set %s' % (i, num, name, set))
+
+        for i in range(num):
+
+            output_fn = '/datax/scratch/bbrzycki/data/%s/%s/%s/%s/%s_%04d.png' % (dir,set,name,'noise','noise',i)
+
+            # level = 0 for no signal
+            signal = stg.generate(ts,
+                                      fs,
+                                      stg.constant_path(f_start = fs[0], drift_rate = 0),
+                                      stg.constant_t_profile(level = 0),
+                                      stg.gaussian_f_profile(width = 0.00002),
+                                      stg.constant_bp_profile(level = 1.0))
+
+            if set == 'raw':
+                signal = signal
+            elif set == 'normalized':
+                signal = stg.normalize(stg.inject_noise(signal), cols = 0, exclude = 0.0, use_median=False)
+            elif set == 'normalized_excluded':
+                signal = stg.normalize(stg.inject_noise(signal), cols = 1, exclude = 0.2, use_median=False)
+
+            plt.imsave(output_fn, signal)
+            print('Saved %s of %s noise data for %s, set %s' % (i, num, name, set))
---
+++ scripts/train_normalized_comparison.py
@@ -0,0 +1,93 @@
+# basic cnn training
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+import tensorflow as tf
+
+# config = tf.ConfigProto()
+# config.gpu_options.per_process_gpu_memory_fraction = 0.2
+# session = tf.Session(config=config)
+
+# dimensions of our images.
+img_width, img_height = 32, 1024
+
+dir = 'normalized_comparison'
+sets = ['raw', 'normalized', 'normalized_excluded']
+for set in sets:
+    
+    print('Working on set %s' % set)
+
+    train_data_dir = '/datax/scratch/bbrzycki/data/%s/%s/train/' % (dir, set)
+    validation_data_dir = '/datax/scratch/bbrzycki/data/%s/%s/validation/' % (dir, set)
+    nb_train_samples = 5000*3
+    nb_validation_samples = 500*3
+    epochs = 50
+    batch_size = 64
+
+    if K.image_data_format() == 'channels_first':
+        input_shape = (3, img_width, img_height)
+    else:
+        input_shape = (img_width, img_height, 3)
+
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(3, activation='softmax'))
+
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    # this is the augmentation configuration we will use for training
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+    # this is the augmentation configuration we will use for testing:
+    # only rescaling
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='sparse')
+
+    validation_generator = test_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='sparse')
+
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch=nb_train_samples // batch_size,
+        epochs=epochs,
+        validation_data=validation_generator,
+        validation_steps=nb_validation_samples // batch_size)
+
+    model_dir = '/datax/scratch/bbrzycki/models/%s/' % dir
+    try:
+        os.makedirs(model_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    model.save_weights(model_dir + '%s.h5' % set)
